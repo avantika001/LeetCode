d74b19faec2653da69213b14f68040c516034fd4
avantika001 <46296562+avantika001@users.noreply.github.com>
2019-10-02 09:04:43 +0530

    Create SubarraySumEqualsK.java

---
+++ SubarraySumEqualsK.java
@@ -0,0 +1,17 @@
+/* Input:nums = [1,1,1], k = 2
+Output: 2 */
+
+public class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        for (int start = 0; start < nums.length; start++) {
+            int sum=0;
+            for (int end = start; end < nums.length; end++) {
+                sum+=nums[end];
+                if (sum == k)
+                    count++;
+            }
+        }
+        return count;
+    }
+}
